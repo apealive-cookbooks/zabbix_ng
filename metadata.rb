a4941fe18c497c9dfaf12f09547c07d4ed08ecc1
Petr Michalec <petr_michalec@cz.ibm.com>
2016-02-16 17:29:12 +0100

    add kitchen and berksfile deps - initial

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,6 @@ license          'GNU Public License 3.0'
 description      'Installs/Configures zabbix_ng'
 long_description 'Installs/Configures zabbix_ng'
 version          '0.1.1'
+
 depends          'apt'
 depends          'sudo'
